90caabb0329824cd7f9c73b9b73294fcb2e39bed
gabriel santos <gabrielsantos98898@gmail.com>
2024-06-19 17:04:38 -0300

    :sparkles: Add Params 'page', 'nextPage' and 'lastPage'

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -7,11 +7,17 @@ interface SendResponseParams {
     message: string;
     data?: any;
     error?: any;
+    page?: any;
+    nextPage?: any;
+    prevPage?: any;
 }
 
-function sendResponse({res, success, statusCode, message, data, error}: SendResponseParams):void {
+function sendResponse({res, success, statusCode, message, data, error, page, nextPage, prevPage}: SendResponseParams):void {
 
     const result: {
+        page?: any;
+        nextPage?: any;
+        prevPage?: any;
         message: string;
         success: boolean;
         status: number;
@@ -22,9 +28,14 @@ function sendResponse({res, success, statusCode, message, data, error}: SendResp
         success,
         status: statusCode
     };
+    
+    if(page) result.page = page;
+    if(nextPage) result.nextPage = nextPage;
+    if(prevPage) result.prevPage = prevPage;
 
     if(data) result.data = data;
     if(error) result.error = error;
+
 
     res.header('Access-Control-Allow-Origin', '*');
     res.status(statusCode).json(result);
