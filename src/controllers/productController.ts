1b0e2fe8d6437c3d095a5b233e5f880bfde20dee
gabriel santos <gabrielsantos98898@gmail.com>
2024-06-19 14:11:38 -0300

    :wrench: Ajustado posicionamento da funcao getPorductCode

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -88,63 +88,6 @@ async function getProductId(req: Request, res: Response): Promise<void> {
 
 }
 
-async function productCreate(req:Request, res:Response) {
-    
-    let message;
-    const {
-        data
-    } = req.body;
-
-    try {
-
-        await createProduct(data);
-
-        message = `Product: ${data.nm_product} registred successfully!`;
-        sendResponse({
-            res,
-            success: true,
-            statusCode: 201,
-            message: message 
-        })
-        
-    } catch (err) {
-        
-        let errorMessage = 'An unknown error occurred';
-        let errorCode:string | undefined;
-
-        if(err instanceof Error){
-            errorMessage = err.message;
-
-            if('code' in err){
-                errorCode = (err as any).code;
-            }
-        }
-
-        if(errorCode === 'ER_DUP_ENTRY') {
-            message = 'Duplication error when registering the product.';
-            sendResponse({
-                res,
-                success: false,
-                statusCode: 500,
-                message: message,
-                error: errorMessage
-            })
-        }else{
-            message = 'Error when trying to list create Product.';
-
-            sendResponse({
-                res,
-                success: true,
-                statusCode: 500,
-                message: message,
-                error: errorMessage
-            })
-        }
-
-    }
-
-}
-
 async function getProductCode(req:Request, res:Response) {
     
     let message;
@@ -203,6 +146,63 @@ async function getProductCode(req:Request, res:Response) {
             error: errorMessage
         })
         
+
+    }
+
+}
+
+async function productCreate(req:Request, res:Response) {
+    
+    let message;
+    const {
+        data
+    } = req.body;
+
+    try {
+
+        await createProduct(data);
+
+        message = `Product: ${data.nm_product} registred successfully!`;
+        sendResponse({
+            res,
+            success: true,
+            statusCode: 201,
+            message: message 
+        })
+        
+    } catch (err) {
+        
+        let errorMessage = 'An unknown error occurred';
+        let errorCode:string | undefined;
+
+        if(err instanceof Error){
+            errorMessage = err.message;
+
+            if('code' in err){
+                errorCode = (err as any).code;
+            }
+        }
+
+        if(errorCode === 'ER_DUP_ENTRY') {
+            message = 'Duplication error when registering the product.';
+            sendResponse({
+                res,
+                success: false,
+                statusCode: 500,
+                message: message,
+                error: errorMessage
+            })
+        }else{
+            message = 'Error when trying to list create Product.';
+
+            sendResponse({
+                res,
+                success: true,
+                statusCode: 500,
+                message: message,
+                error: errorMessage
+            })
+        }
 
     }
 
