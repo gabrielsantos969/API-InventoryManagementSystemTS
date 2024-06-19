9f00fb586692dadf155ef01f3d89bd5b5df48ee9
gabriel santos <gabrielsantos98898@gmail.com>
2024-06-19 17:15:14 -0300

    :wrench: Configured the connection by .env

--- src/config/connect.ts
+++ src/config/connect.ts
@@ -1,11 +1,13 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const con = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'inventory_manager_system',
-    port: 3306
-})
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'inventory_manager_system',
+    port: Number(process.env.DB_PORT) || 3306,
+  });
 
 export default con;
