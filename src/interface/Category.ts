c5442252cf8ca54d173006f912bfd51adc5a3f73
gabriel santos <gabrielsantos98898@gmail.com>
2024-06-26 11:44:48 -0300

    :wrench: Name adjustment in the interface

--- src/interface/Category.ts
+++ src/interface/Category.ts
@@ -1,14 +1,14 @@
 export default class Category {
 
     id?: number;
-    nm_categroy: string;
+    nm_category: string;
     sn_active: string;
     created_at?: Date;
     updated_at?: Date;
 
     constructor(nm_category: string, sn_active: string, id?:number, created_at?: Date, updated_at?: Date){
         this.id = id;
-        this.nm_categroy = nm_category;
+        this.nm_category = nm_category;
         this.sn_active = sn_active;
         this.created_at = created_at;
         this.updated_at = updated_at;
