3ccb18864ded6ed9ba801df36d7f4d627dfc9358
gabriel santos <gabrielsantos98898@gmail.com>
2024-06-27 10:38:57 -0300

    :sparkles: Code improvements

--- src/models/productCategoryModel.ts
+++ src/models/productCategoryModel.ts
@@ -1,8 +1,10 @@
 import con from '../config/connect';
 
-const addCategoryInProduct = async (id_product:number, id_category: any): Promise<void> => {
+const addCategoryInProduct = async (id_product:number, categoryIds: number[]): Promise<void> => {
 
-    await con.promise().query("INSERT INTO product_category (id_product, id_category) VALUES (?, ?)", [id_product, id_category]);
+    const productCategoryValues = await categoryIds.map((categoryId:number) => [id_product, categoryId]);
+
+    await con.promise().query("INSERT INTO product_category (id_product, id_category) VALUES ?", [productCategoryValues]);
 
 }
 
