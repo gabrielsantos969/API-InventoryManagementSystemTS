910c635d870668a6975ba092094796ba69af1948
gabriel santos <gabrielsantos98898@gmail.com>
2024-06-28 15:07:30 -0300

    :sparkles: Add all routes Users

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,8 +1,22 @@
 import { Router } from "express"; 
-import { getById } from "../controllers/userController";
+import { getAll, getByEmail, getById, getByName, getByUsername, create, update, deleteUser } from "../controllers/userController";
 
 const router = Router();
 
+router.get('/v1/getAll', getAll);
+
 router.get('/v1/getById/:id', getById);
+
+router.get('/v1/getByUsername/:username', getByUsername);
+
+router.get('/v1/getByName/:name', getByName);
+
+router.get('/v1/getByEmail/:email', getByEmail);
+
+router.post('/v1/create', create);
+
+router.put('/v1/update/:id', update);
+
+router.delete('/v1/delete/:id', deleteUser);
 
 export default router;
